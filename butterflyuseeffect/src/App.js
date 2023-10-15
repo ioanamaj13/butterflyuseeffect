76497edfa79bd61864b5f0cc1dec49bc264047d4
Ioana Sauleac <ioana@Ioanas-MacBook-Pro.local>
2023-10-15 17:18:16 +0100

    init

---
+++ butterflyuseeffect/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Butterfly useEffect
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
